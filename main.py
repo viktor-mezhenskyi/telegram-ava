0ea6eaed28a9f63eff36c8cf142999a4aa83f745
Viktor <viktor@gmail.com>
2022-03-07 09:46:32 +0200

    Add blocker logic

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+from telethon import TelegramClient
+from telethon import functions, types
+from config import api_id, api_hash
+
+channels = open('channels.txt','r').read().splitlines()
+
+async def main():
+    async with TelegramClient('session', api_id, api_hash) as client:
+        for channel_name in channels:
+            result = await client(functions.contacts.SearchRequest(
+                q=channel_name,
+                limit=1
+            ))
+            id = result.results[0].channel_id
+            result = await client(functions.messages.ReportRequest(
+                peer='channel',
+                id=[id],
+                reason=types.InputReportReasonOther(),
+                message='Russian aggression'
+            ))
+            print(result)      
+asyncio.run(main())
