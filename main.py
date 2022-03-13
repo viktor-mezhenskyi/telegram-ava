ded44cccdf7e80709e54b6270ec5fa0697697866
Viktor <viktor@gmail.com>
2022-03-13 21:20:58 +0200

    Refactor code

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import asyncio
+import re
 from telethon import TelegramClient
 from telethon import functions, types
 from config import api_id, api_hash
 
 channels = open('channels.txt','r', encoding="utf8").read().splitlines()
 
+async def search_channel(client, name):
+    result = await client(functions.contacts.SearchRequest(
+             q=name,
+             limit=1
+            ))
+    return result.results
+
+async def ban_channel(client, id):
+    response = await client(functions.messages.ReportRequest(
+                   peer='channel',
+                   id=[id],
+                   reason=types.InputReportReasonOther(),
+                   message='Russian aggression'
+                ))
+    return response
+
 async def main():
     async with TelegramClient('session', api_id, api_hash) as client:
         for channel_name in channels:
             #normalize channel name
             channel_name=channel_name.replace("https://t.me/", "")
-            result = await client(functions.contacts.SearchRequest(
-                q=channel_name,
-                limit=1
-            ))
-            
+            found_channels = await search_channel(client, channel_name)
+
             #try ban channel if found
-            if len(result.results) > 0:
-                id = result.results[0].channel_id
-                response = await client(functions.messages.ReportRequest(
-                    peer='channel',
-                    id=[id],
-                    reason=types.InputReportReasonOther(),
-                    message='Russian aggression'
-                ))
+            if len(found_channels) > 0:
+                id = found_channels[0].channel_id
+                response = await ban_channel(client, id)
                 if response:
                     print("{} was banned".format(channel_name))
                 else:
@@ -31,3 +40,4 @@ async def main():
             else:
                 print("{} was NOT found".format(channel_name))                
 asyncio.run(main())
+
